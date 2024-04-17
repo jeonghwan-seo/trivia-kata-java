369728db1e3b012145d9696acc4b86ca06dae51b
hwany <jeonghwan.seo@mincoding.co.kr>
2024-04-17 15:21:44 +0900

    first commit

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+    @Test
+    public void true_is_true() throws Exception {
+        assertTrue(false);
+    }
+
+}
